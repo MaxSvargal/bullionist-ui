b00c3f2f0a055e818b30f273240a1ac0e48dcbb9
MaxSvargal <maxsvargal@gmail.com>
2018-04-12 19:04:22 +0300

    Fix commission calculation and redesign Position Closed List, extend chunk amount info

--- pages/components/positionsClosedList.tsx
+++ pages/components/positionsClosedList.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import glamorous, { Div } from 'glamorous'
 import moment from 'moment'
-import { filter, prop, propEq, o, sortBy, path, reverse, converge, add, lt } from 'ramda'
-import { toFixed } from '../shared/helpers'
+import { filter, prop, propEq, o, sortBy, path, reverse, converge, add, lt, multiply, head, last } from 'ramda'
+import { toFixed, takePairFromSymbol } from '../shared/helpers'
 
+const toFixed4 = toFixed(4)
 const toFixed8 = toFixed(8)
-const comissionsSumm = converge(add, [ path([ 'open', 'comission' ]), path([ 'close', 'comission' ]) ])
+const symbolPair = o(takePairFromSymbol, prop('symbol'))
+const chunkAmount = converge(multiply, [ path([ 'open', 'origQty' ]), path([ 'open', 'price' ]) ])
+const comissionsSumm = converge(add, [ path([ 'open', 'commission' ]), path([ 'close', 'commission' ]) ])
 const filterClosed = o(o(reverse, sortBy(path([ 'close', 'time' ]))), filter(propEq('closed', true)))
 
 type Props = { positions: {}[] }
@@ -14,10 +17,16 @@ export default class extends Component<Props> {
     const { positions } = this.props
     const closed = filterClosed(positions)
 
+    const Container = glamorous.div({
+      display: 'grid'
+    })
+
     const Item = glamorous.div(({ isPositive }) => ({
-      display: 'flex',
+      boxSizing: 'border-box',
+      display: 'grid',
+      gridColumn: '1 / -1',
+      gridTemplateColumns: '1.25fr 1.25fr 1.5fr 1fr',
       alignItems: 'center',
-      justifyContent: 'space-between',
       lineHeight: '1.8rem',
       padding: '.5rem 1rem',
       color: '#546e7a',
@@ -28,37 +37,30 @@ export default class extends Component<Props> {
         fontSize: '.65rem',
         lineHeight: '1.25rem',
         padding: '.25rem .5rem',
-        maxHeight: '4rem'
+        maxHeight: '8rem',
+        gridTemplateColumns: '1fr 1.2fr 2fr'
       }
     }))
     const Symbol = glamorous.div({
       fontSize: '3em',
       color: '#fff',
-      flex: '0 0 15rem',
       '@media(max-width: 600px)': {
-        fontSize: '2em',
-        flex: '0 0 6rem'
+        fontSize: '2em'
       }
     })
     const Profit = glamorous.div({
-      fontSize: '1.2em',
-      flex: '0 0 13rem',
-      '@media(max-width: 600px)': {
-        flex: '0 0 8rem'
-      }
+      fontSize: '1.2em'
     })
     const Prices = glamorous.div({
-      flex: '0 0 16rem',
-      '@media(max-width: 600px)': {
-        flex: '0 0 10.5rem'
-      }
+      padding: '0 .5rem'
     })
     const Label = glamorous.div({
       fontSize: '.8em',
       marginBottom: '0.15em'
     })
     const Bigger = glamorous.span({
-      fontSize: '1.2em'
+      fontSize: '1.2em',
+      padding: '0 .5rem'
     })
     const HideWhenSmall = glamorous.div({
       '@media(max-width: 600px)': {
@@ -67,7 +69,7 @@ export default class extends Component<Props> {
     })
 
     return (
-      <Div display='flex' flexFlow='column nowrap'>
+      <Container>
         { closed.map(v => (
           <Item key={ prop('id', v) } isPositive={ o(lt(0), prop('profitPerc'), v) }>
             <Symbol>{ prop('symbol', v) }</Symbol>
@@ -89,12 +91,12 @@ export default class extends Component<Props> {
             </Prices>
             <HideWhenSmall>
               <Label>Amount / Comission</Label>
-              <Div>{ path([ 'open', 'origQty' ], v) } { prop('symbol', v) }</Div>
+              <Div>{ path([ 'open', 'origQty' ], v) } { o(head, symbolPair, v) } | { o(toFixed4, chunkAmount, v) } { o(last, symbolPair, v) }</Div>
               <Div>{ o(toFixed8, comissionsSumm, v) } { path([ 'close', 'commissionAsset' ], v) }</Div>
             </HideWhenSmall>
           </Item>
         )) }
-      </Div>
+      </Container>
     )
   }
 }
--- pages/shared/helpers.ts
+++ pages/shared/helpers.ts
@@ -1,5 +1,8 @@
-import { o, curry, invoker } from 'ramda'
+import { pair, invoker, compose, converge, nth, match } from 'ramda'
 
 type ToFixed = (percision?: number) => (num?: number) => string
 export const toFixed: ToFixed = (per = 4) => (num = 0) =>
   num ? invoker(1, 'toFixed')(per, num) : '0'
+
+type TakePairFromSymbol = (a: string) => [ string, string ]
+export const takePairFromSymbol: TakePairFromSymbol = compose(converge(pair, [ nth(1), nth(2) ]), match(/(.+)(...)/))
